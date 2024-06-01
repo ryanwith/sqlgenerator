8001b1391289a6bf6e1a0cf18d7d72d0fddd06a6
Ryan Waldorf <ryanwaldorf@Ryans-MacBook-Pro-2.local>
2024-06-01 10:48:26 -0400

    fix semi colons

--- src/utils/sqlGenerator.js
+++ src/utils/sqlGenerator.js
@@ -13,25 +13,41 @@ export const generateCreateTableSQL = (headers, table_name, column_type, temp_ta
 export const generateInsertStatements = (data, table_name, batch_size) => {
   const headers = data[0];
   const rows = data.slice(1);
+  console.log(rows.length);
   const insertIntoClause = generateInsertIntoClause(table_name);
   const insertStatements = rows.map((row, row_number) => {
     const values = row.map((value) => `'${value}'`).join(', ');
-    // add the insertIntoClause on the first row if the batch size is not set
-    if(batch_size === undefined && row_number === 0){
-      return `${insertIntoClause}\n\t(${values})`
-    } 
-    // add the insertIntoClause on the first row of each batch if the batch size is set
-    else if(row_number%batch_size === 0){
-      return `${insertIntoClause}\n\t(${values})`
-    } 
-    // return just the values otherwise
-    else {
-      return `(${values})`
-    }
+
+    return generateValuesStatement(insertIntoClause, values, rows.length, row_number, batch_size)
   });
-  return insertStatements.join('\n\t');
+  return insertStatements.join('');
 };
 
 export const generateInsertIntoClause = (table_name) => {
   return `INSERT INTO ${table_name} VALUES`
+}
+
+export const generateValuesStatement = (insertIntoClause, values, total_rows, row_number, batch_size) => {
+  let statement = '';
+  // add the insertIntoClause on the first row if the batch size is not set
+  if(batch_size === undefined && row_number === 0){
+    statement = `${insertIntoClause}\n\t(${values})`
+  } 
+  // add the insertIntoClause on the first row of each batch if the batch size is set
+  else if(row_number%batch_size === 0){
+    statement =  `${insertIntoClause}\n\t(${values})`
+  } 
+  // return just the values otherwise
+  else {
+    statement =  `\n\t(${values})`
+  }
+  
+  if(row_number === total_rows - 1 || 
+    (batch_size != undefined &&row_number % batch_size === batch_size - 1)
+  ){    
+    statement = statement + ';'
+
+  }
+  return statement;
+
 }
