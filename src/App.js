c599859789bd355a212749b9d36580d9ce40199a
Ryan Waldorf <ryanwaldorf@Ryans-MacBook-Pro-2.local>
2024-06-02 15:00:31 -0400

    basically have it working

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import FileUpload from './components/FileUpload';
 import SQLDisplay from './components/SQLDisplay';
 import { generateAllStatements } from './utils/sqlGenerator';
-import { Container, Typography, Grid, Box, InputLabel, Select, FormHelperText, FormControl, MenuItem} from '@mui/material';
+import { Container, Typography, Grid, Box, InputLabel, Select, FormHelperText, FormControl, MenuItem, TextField} from '@mui/material';
 
 function App() {
   const [sql, setSQL] = useState('');
@@ -11,61 +11,122 @@ function App() {
   const [tableType, setTableType] = useState('TEMP');
   const [tableName, setTableName] = useState('table_name');
   const [columnType, setColumnType] = useState('VARCHAR');
-  const [batchSize, setBatchSize] = useState(null);
+  const [batchSize, setBatchSize] = useState("");
   const [fileData, setFileData] = useState(null);
 
-  const handleData = (data) => {
-    setFileData(data);
+  const handleData = (data) => { setFileData(data) };
+
+  const handleSQLCriteriaChange = (event) => {
+    const { name, value } = event.target;
+    switch (name) {
+      case 'tableType':
+        setTableType(value);
+        break;
+      case 'tableName':
+        setTableName(value);
+        break;
+      case 'columnType':
+        setColumnType(value);
+        break;
+      case 'batchSize':
+        setBatchSize(value);
+        break;
+      default:
+        break;
+    }
   };
+
+
+
 
   useEffect(() => {
     if (fileData !== null && (fileData || tableName || columnType || tableType || batchSize !== null)) {
       const allStatements = generateAllStatements(fileData, tableName, columnType, tableType, batchSize);
-      const newSQL = allStatements.join("\n\n");
+      const newSQL = allStatements.join();
       setSQL(newSQL);
       setIsVisible(!!newSQL); // Show SQLDisplay if newSQL is not empty
     }
   }, [fileData, tableName, columnType, tableType, batchSize]);
 
-  const handleTableTypeChange = (event) => {
-    setTableType(event.target.value);
-    // handleData(data, tableType)
-  };
 
 
 return (
   <Container>
     <h1>Excel to SQL</h1>
     <Grid container spacing={2}>
-      <Grid item xs={8}>
+      <Grid item xs="10">
           Easily convert and transform your excel, CSVs, TSVs, and other data files to SQL. Allows you to easily extract, transform, and load small amounts of data between data warehouses.
       </Grid>
-      <Grid item xs={2}>
+      <Grid item xs="auto">
         <FileUpload onData={handleData} />
       </Grid>
-      <Grid item xs={10}>
+      <Grid item xs={12}>
         <Box mt={2}>
           {isVisible && <SQLDisplay sql={sql} onChange={handleData} />}
         </Box>
       </Grid>
-      <Grid item xs={10}>
+      <Grid id='sql-criteria' item xs={12}>
         <Box mt={2}>
-        <FormControl fullWidth margin="normal">
-            <InputLabel id="table-type">Table Type</InputLabel>
-            <Select
-              labelId="table-type"
-              value={tableType}
-              onChange={handleTableTypeChange}
-            >
-              <MenuItem value='TEMP'>Temp Table</MenuItem>
-              <MenuItem value='PERM'>Permanent Table</MenuItem>
-            </Select>
-          </FormControl>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <InputLabel id="table-type">Table Type</InputLabel>
+                <Select
+                  labelId="table-type"
+                  id='table-type-select'
+                  name='tableType'
+                  value={tableType}
+                  label='Table Type'
+                  onChange={handleSQLCriteriaChange}
+                >
+                  <MenuItem value='TEMP'>Temp Table</MenuItem>
+                  <MenuItem value='PERM'>Permanent Table</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <TextField
+                  id="table-name"
+                  label="Table Name"
+                  name='tableName'
+                  value={tableName}
+                  onChange={handleSQLCriteriaChange}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <TextField
+                  id="column-type"
+                  label="Column Type"
+                  name='columnType'
+                  value={columnType}
+                  onChange={handleSQLCriteriaChange}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <TextField
+                  id="batch-size"
+                  label="Batch Size"
+                  type="number"
+                  name='batchSize'
+                  value={batchSize}
+                  onChange={handleSQLCriteriaChange}
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
         </Box>
       </Grid>
     </Grid>
+
   </Container>
 );
 
 }
 export default App;
+
+
--- src/utils/sqlGenerator.js
+++ src/utils/sqlGenerator.js
@@ -5,52 +5,69 @@ export const generateAllStatements = (data, tableName, columnType, tableType, ba
   ].flat()
 }
 
-export const generateCreateTableSQL = (headers, tableName, columnType, tableType) => {
+const generateCreateTableSQL = (headers, tableName, columnType, tableType) => {
   const columns = headers.map((header) => `${header} ${columnType}`).join(', ');
-  console.log(`generateCreateTableSQL tableType = ${tableType}`)
   const createTableSQL = `CREATE ${tableType === 'TEMP' ? "TEMP " : ""}TABLE ${tableName} (${columns});`;
-  console.log(`SQL: ${createTableSQL}`)
   return createTableSQL;
-};
+}
 
-export const generateInsertStatements = (data, tableName, batchSize) => {
-  const headers = data[0];
+const generateInsertStatements = (data, tableName, batchSize) => {
   const rows = data.slice(1);
-  console.log(rows.length);
   const insertIntoClause = generateInsertIntoClause(tableName);
   const insertStatements = rows.map((row, rowNumber) => {
-    const values = row.map((value) => `'${value}'`).join(', ');
-
-    return generateValuesStatement(insertIntoClause, values, rows.length, rowNumber, batchSize)
+    return generateInsertLine(insertIntoClause, row, rows.length, rowNumber, batchSize)
   });
   return insertStatements.join('');
 };
 
-export const generateInsertIntoClause = (tableName) => {
-  return `INSERT INTO ${tableName} VALUES`
+const generateInsertIntoClause = (tableName) => {
+  return `\n\nINSERT INTO ${tableName} VALUES`
 }
 
-export const generateValuesStatement = (insertIntoClause, values, totalRows, rowNumber, batchSize) => {
-  let statement = '';
-  // add the insertIntoClause on the first row if the batch size is not set
-  if(batchSize === null && rowNumber === 0){
-    statement = `${insertIntoClause}\n\t(${values})`
+const generateInsertLine = (insertIntoClause, row, totalRows, rowNumber, batchSize) => {
+  const beginningOfStatement = isFirstLineOfStatement(rowNumber, batchSize) ? insertIntoClause : '';
+  const values = "\n\t(" + row.map((value) => `'${value}'`).join(', ') + ")";
+  const endOfStatement = isEndOfStatement(totalRows, rowNumber, batchSize) ? ";" : ",";
+  return beginningOfStatement + values + endOfStatement;
+}
+
+
+// console.log(commaOrSemicolon(totalRows, rowNumber, batchSize))
+// console.log(isEndOfStatement(totalRows, rowNumber, batchSize) ? "slash n" : "")
+// console.log(`end of statement: ${endOfStatement}`)
+const isEndOfStatement = (totalRows, rowNumber, batchSize) => {
+  // last row or end of batch
+    if(
+      // last row 
+      rowNumber === totalRows - 1
+      // valid batch size and end of batch
+      || ( validBatchSize(batchSize) && rowNumber % batchSize === batchSize - 1)
+    ){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+// HELPERS
+
+const isFirstLineOfStatement = (rowNumber, batchSize) => {
+  // if it is the first row
+  if(rowNumber === 0){
+    return true;
   } 
-  // add the insertIntoClause on the first row of each batch if the batch size is set
-  else if(rowNumber%batchSize === 0){
-    statement =  `${insertIntoClause}\n\t(${values})`
-  } 
-  // return just the values otherwise
+  // if it is a new barch
+  else if (validBatchSize(batchSize) && rowNumber % batchSize === 0) 
+    return true;
   else {
-    statement =  `\n\t(${values})`
+    return false;
   }
-  
-  if(rowNumber === totalRows - 1 || 
-    (batchSize != null &&rowNumber % batchSize === batchSize - 1)
-  ){    
-    statement = statement + ';'
+}
 
+const validBatchSize = (batchSize) => {
+  if(Number.isInteger(batchSize) !== true && batchSize > 0){
+    return true;
+  } else {
+    return false;
   }
-  return statement;
-
 }
