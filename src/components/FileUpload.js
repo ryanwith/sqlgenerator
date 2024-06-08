fa2acfff7aa11124470fa303bad324aeb8c614ac
Ryan Waldorf <ryanwaldorf@Ryans-MacBook-Pro-2.local>
2024-06-08 12:08:38 -0400

    remove unneeded dependency

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button } from '@mui/material';
 import * as XLSX from 'xlsx';
 import {formatJSON} from '../utils/jsonFormatter'
