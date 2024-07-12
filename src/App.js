0e5ae3f245da5ea001f40a256fda6e8c57d1e5a7
Ryan Waldorf <ryanwaldorf@Ryans-MacBook-Pro-2.local>
2024-07-12 10:03:15 -0400

    did some cleanup

--- src/App.js
+++ src/App.js
@@ -27,35 +27,3 @@ function App() {
 }
 
 export default App;
-
-
-// import React from 'react';
-// import { HashRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
-// import InClauseGenerator from './pages/InClauseGenerator';
-// import TableGeneratorFromExcel from './pages/TableGeneratorFromExcel';
-// import TableGeneratorFromJSON from './pages/TableGeneratorFromJSON';
-// import Home from './pages/Home';
-// import Navbar from './components/Navbar';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <Router>
-//       <div>
-//         <Navbar></Navbar>
-
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/sql-in-clause-generator" element={<InClauseGenerator />} />
-//           <Route path="/sql-converter/excel-to-sql" element={<TableGeneratorFromExcel />} />
-//           <Route path="/sql-converter/json-to-sql" element={<TableGeneratorFromJSON />} />
-//           <Route path="/in-clause-generator" element={<Navigate to="/sql-in-clause-generator" />} />
-//           {/* Add a catch-all redirect */}
-//           <Route path="*" element={<Navigate to="/" />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
