d5b8d71a8dc6454d17378276ac3cb1207625116e
Ryan Waldorf <ryan.m.waldorf@gmail.com>
2024-08-16 16:41:17 -0400

    working alter statement generator

--- src/pages/statements/AlterStatementsGenerator.js
+++ src/pages/statements/AlterStatementsGenerator.js
@@ -1,91 +1,73 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import * as XLSX from 'xlsx';
-import { TextField, Button, Box, Tabs, Tab, Container, Typography, Grid, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
-import sqlGenerator from '../../utils/sqlGenerator';
-const { generateInClausesFromPaste, generateAlterStatements  } = sqlGenerator;
+import { TextField, Button, Box, Tabs, Tab, Container, Typography, Grid, FormControl } from '@mui/material';
 
-function InClauseGenerator() {
+function AlterStatementsGenerator() {
   const [excelData, setExcelData] = useState('');
-  const [parsedData, setParsedData] = useState([]);
   const [alterStatements, setAlterStatements] = useState('');
   const [alterStatementBeginning, setAlterStatementBeginning] = useState('ALTER TABLE');
   const [alterStatementEnd, setAlterStatementEnd] = useState('REPLICA IDENTITY FULL');
-  const [objectBeginningDelimiter, setObjectBeginningDelimiter] = useState('"');
-  const [objectEndDelimiter, setObjectEndDelimiter] = useState('"');
-  const [notIn, setNotIn] = useState(false);
-  const [batchSize, setBatchSize] = useState(0);
-  const [currentTab, setCurrentTab] = useState(0); // 0 for Pasted Data, 1 for In Clause
-  const [unformattedClauses, setUnformattedClauses] = useState([]); // 0 for Pasted Data, 1 for In Clause
-  const [disableButtons, setDisableButtons] = useState(true);
+  const [beginningDelimiter, setBeginningDelimiter] = useState('"');
+  const [endDelimiter, setEndDelimiter] = useState('"');
+  const [currentTab, setCurrentTab] = useState(0); // 0 for Pasted Data, 1 for Alter Statements
 
   const handleTabChange = (event, newValue) => {
     setCurrentTab(newValue);
   };
 
   const handlePaste = (event) => {
-    const pastedText = event.target.value;
-    setExcelData(pastedText);
+    setExcelData(event.target.value);
   };
 
-  const parseExcelData = () => {
-    // Split the pasted text by new lines and then by tabs or other delimiters
+  const generateAlterStatements = () => {
     const rows = excelData.trim().split('\n').map(row => row.split('\t'));
-
-    // Create a worksheet from the rows
-    const sheet = XLSX.utils.aoa_to_sheet(rows);
-    const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
-    setParsedData(jsonData);
-    setUnformattedClauses(generateInClausesFromPaste(jsonData, batchSize));
-    setAlterStatements(generateAlterStatements(unformattedClauses));
-    setCurrentTab(1);
+    const statements = rows.map(row => {
+      const formattedColumns = row.map(col => `${beginningDelimiter}${col}${endDelimiter}`).join('.');
+      return `${alterStatementBeginning} ${formattedColumns} ${alterStatementEnd};`;
+    }).join('\n');
+    setAlterStatements(statements);
+    setCurrentTab(1); // Switch to the tab with the generated SQL statements
   };
-
-  useEffect(() => {
-    if (parsedData.length !== 0) {
-      const clauses = generateInClausesFromPaste(parsedData, batchSize);
-      setUnformattedClauses(clauses);
-    }
-  }, [parsedData, batchSize]);
-
-  useEffect(() => {
-    if (excelData){
-      setDisableButtons(false);
-    } else {
-      setDisableButtons(true);
-    }
-  }, [excelData])
 
   const handleSQLCriteriaChange = (event) => {
     const { name, value } = event.target;
-    if (name === 'alterStatementBeginning') setAlterStatementBeginning(value);
-    if (name === 'alterStatementEnd') setAlterStatementEnd(value);
-    // if (name === 'notIn') setNotIn(value === 'EXCLUDE' ? true : false);
-    // if (name === 'batchSize') setBatchSize(value);
+    switch (name) {
+      case 'alterStatementBeginning':
+        setAlterStatementBeginning(value);
+        break;
+      case 'alterStatementEnd':
+        setAlterStatementEnd(value);
+        break;
+      case 'beginningDelimiter':
+        setBeginningDelimiter(value);
+        break;
+      case 'endDelimiter':
+        setEndDelimiter(value);
+        break;
+      default:
+        break;
+    }
   };
-
-
-
 
   return (
     <Container>
       <Typography variant="h1">Alter Statements Generator</Typography>
-      <Typography variant='body1' style={{"margin-top": '4px'}}>
-        Copy and paste a list of tables you want to generate alter statements for from excel.  This will generate alter statements for each table matching the format you provide below.
+      <Typography variant="body1" style={{ marginTop: '4px' }}>
+        Paste Excel data here and generate SQL ALTER statements with custom delimiters and formatting.
       </Typography>
       <Box>
         <Tabs 
-            mb={2} 
-            value={currentTab} 
-            onChange={handleTabChange}
-            >
-            <Tab label="Pasted Data" />
-            <Tab label="Generated Alter Statements" />
-            
+          value={currentTab} 
+          onChange={handleTabChange}
+          indicatorColor="primary"
+          textColor="primary"
+          style={{ marginBottom: '10px' }}
+        >
+          <Tab label="Pasted Data" />
+          <Tab label="Generated Alter Statements" />
         </Tabs>
-      </Box>
-      {currentTab === 0 && (
-        <Box>
-          <TextField 
+        {currentTab === 0 && (
+          <TextField
             label="Paste Excel Data Here"
             multiline
             rows={10}
@@ -93,90 +75,75 @@ function InClauseGenerator() {
             fullWidth
             value={excelData}
             onChange={handlePaste}
-            style={{ marginTop: '10px', zIndex: 1 }} // Ensure it stays above other elements
-
+            style={{ marginBottom: '10px' }}
           />
-        </Box>
-      )}
-      {currentTab === 1 && (
-        <Box>
+        )}
+        {currentTab === 1 && (
           <TextField
-            label="Alter Statements"
+            label="Generated Alter Statements"
             multiline
             rows={10}
             variant="outlined"
             fullWidth
             value={alterStatements}
-            onChange={(e) => setAlterStatements(e.target.value)}
-            style={{ marginTop: '10px', zIndex: 1 }} // Ensure it stays above other elements
+            readOnly
+            style={{ marginBottom: '10px' }}
           />
-        </Box>
-      )}
-      <Grid id='sql-criteria' item xs={12}>
-        <Box mt={2}>
-          <Grid container spacing={2}>
-            <Grid item sm={4} xs={12}>
-              <FormControl fullWidth>
-                <TextField
-                  labelId="alter-statement-beginning-label"
-                  id="alter-statement-beginning-input"
-                  label="Alter Statement Beginning"
-                  name='alterStatementBeginning'
-                  value={alterStatementBeginning}
-                  onChange={handleSQLCriteriaChange}
-                />
-              </FormControl>
-            </Grid>
-            <Grid item sm={4} xs={12}>
-              <FormControl fullWidth>
-                <TextField
-                  labelId="alter-statement-end-label"
-                  id="alter-statement-end-input"
-                  label="Alter Statement End"
-                  name='alterStatementEnd'
-                  value={alterStatementEnd}
-                  onChange={handleSQLCriteriaChange}
-                />
-              </FormControl>
-            </Grid>
-            <Grid item sm={2} xs={12}>
-              <FormControl fullWidth>
-                <TextField
-                  labelId="object-beginning-delimiter-label"
-                  id="object-beginning-delimitert"
-                  label="Object Beginning Delimiter"
-                  name='objectBeginningDelimiter'
-                  value={objectBeginningDelimiter}
-                  onChange={handleSQLCriteriaChange}
-                />
-              </FormControl>
-            </Grid>
-            <Grid item sm={2} xs={12}>
-              <FormControl fullWidth>
-                <TextField
-                  labelId="object-end-delimiter-label"
-                  id="object-end-delimitert"
-                  label="Object End Delimiter"
-                  name='objectEndDelimiter'
-                  value={objectEndDelimiter}
-                  onChange={handleSQLCriteriaChange}
-                />
-              </FormControl>
-            </Grid>
-          </Grid>
-        </Box>
-        <Button
-            variant="contained"
-            color="primary"
-            onClick={parseExcelData}
-            style={{ marginTop: '10px' }}
-            disabled={disableButtons}
-          >
-            Generate
-        </Button>
+        )}
+      </Box>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <FormControl fullWidth>
+            <TextField
+              label="Alter Statement Beginning"
+              name="alterStatementBeginning"
+              value={alterStatementBeginning}
+              onChange={handleSQLCriteriaChange}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={6}>
+          <FormControl fullWidth>
+            <TextField
+              label="Alter Statement End"
+              name="alterStatementEnd"
+              value={alterStatementEnd}
+              onChange={handleSQLCriteriaChange}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={6}>
+          <FormControl fullWidth>
+            <TextField
+              label="Beginning Delimiter"
+              name="beginningDelimiter"
+              value={beginningDelimiter}
+              onChange={handleSQLCriteriaChange}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={6}>
+          <FormControl fullWidth>
+            <TextField
+              label="End Delimiter"
+              name="endDelimiter"
+              value={endDelimiter}
+              onChange={handleSQLCriteriaChange}
+            />
+          </FormControl>
+        </Grid>
       </Grid>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={generateAlterStatements}
+        disabled={!excelData}
+        style={{ marginTop: '10px' }}
+      >
+        Generate
+      </Button>
     </Container>
   );
 }
 
-export default InClauseGenerator;
+export default AlterStatementsGenerator;
